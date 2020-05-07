b34f7021f7a3d46aa2875bf7965802c08db90d83
Touki Sano <touki0213@icloud.com>
2020-05-07 23:23:10 +0900

    Make relationships_table & UserController & user file in view

---
+++ database/migrations/2020_05_07_141230_create_relationships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRelationshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relationships', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('follower_id');
+            $table->unsignedInteger('followed_id');
+            $table->foreign('follower_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('followed_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('relationships');
+    }
+}
